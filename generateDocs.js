c9c35bab27540762cff36ff6a87ca6a1799ba123
davidedc <davidedc@gmail.com>
2017-02-08 17:54:06 +0000

    isolating tags from header comments

--- generateDocs.js
+++ generateDocs.js
@@ -23,6 +23,7 @@ for (var i=0; i<items.length; i++) {
       generatedTiddler += "title: " + items[i] + "\n\n"
 
 
+      // isolate header comments
       const regex = /###\w*=*[\S\s]*?###/mg;
       let m;
       while ((m = regex.exec(contents)) !== null) {
@@ -34,8 +35,32 @@ for (var i=0; i<items.length; i++) {
           // The result can be accessed through the `m`-variable.
           m.forEach((match, groupIndex) => {
               //console.log(`Found match, group ${groupIndex}: ${match}`);
-              generatedTiddler += match.replace(/###\s*/,"").replace(/=====*/,"");
+              generatedTiddler = match.replace(/###\s*/,"").replace(/=====*/,"");
+
+              // isolate tags
+              var tags = [];
+              const regexTags = /Tags\s*-*([\S\s]*?)^[\w ]*\s---/mg;
+              let m2;
+              while ((m2 = regexTags.exec(generatedTiddler)) !== null) {
+                  // This is necessary to avoid infinite loops with zero-width matches
+                  if (m2.index === regexTags.lastIndex) {
+                      regexTags.lastIndex++;
+                  }
+                  
+                  // The result can be accessed through the `m2`-variable.
+                  m2.forEach((match2, groupIndex2) => {
+                      if (groupIndex2 != 0) {
+                      //console.log(`Found match, group ${groupIndex2}: ${match2}`);
+                      tags = match2.split(',').map(eachString => eachString.trim().replace(/ /g,"_"));
+                      console.log("tags: " + tags.toString());
+                    }
+                  });
+              }
+
           });
+
+
+
       }
 
       fs.writeFileSync(items[i] + ".tid", generatedTiddler)
